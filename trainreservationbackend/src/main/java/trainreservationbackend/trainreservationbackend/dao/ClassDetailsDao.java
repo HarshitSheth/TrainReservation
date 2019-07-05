71cd613c2b1a2cf69b3b96a662a315a9b29d84f0
HarshitSheth <harshitsheth298@gmail.com>
2019-07-05 15:20:19 +0530

    Updated for Vagrant support

--- trainreservationbackend/src/main/java/trainreservationbackend/trainreservationbackend/dao/ClassDetailsDao.java
+++
@@ -1,7 +0,0 @@
-package trainreservationbackend.trainreservationbackend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import trainreservationbackend.trainreservationbackend.model.ClassDetails;
-
-public interface ClassDetailsDao extends JpaRepository<ClassDetails, String> {
-}
