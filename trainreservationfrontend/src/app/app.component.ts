ef83a77db82d83cd9a8ce9b63e04e11d2bfe04da
HarshitSheth <harshitsheth298@gmail.com>
2019-07-16 13:01:54 +0530

    changed the localhost url

--- trainreservationfrontend/src/app/app.component.ts
+++ trainreservationfrontend/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 
 
 export class AppComponent {
-  springBoot = 'http://localhost:8081';
+  springBoot = 'http://localhost:8080';
   title = 'Welcome to Train Reservation!';
 }
 
